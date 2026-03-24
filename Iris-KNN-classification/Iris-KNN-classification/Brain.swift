6f2e60a8344724c621c38f25181619e2717b61bc
Viktoriia <savchukv2006@gmail.com>
2026-03-24 12:50:13 +0100

    Rename Iris to Sample struct, add Brain struct with a main logic of my project (methods)

---
+++ Iris-KNN-classification/Iris-KNN-classification/Brain.swift
@@ -0,0 +1,22 @@
+//
+//  Brain.swift
+//  Iris-KNN-classification
+//
+//  Created by Viktoriia Savchuk  on 24/03/2026.
+//
+
+import Foundation
+
+struct Brain {
+    
+    func loadData() {} //Reads input data
+    
+    func euclidianDistance() {} //Calculates how similar two flowers are
+    
+    func classify() {} //Predicts the class of a new flower
+    
+    func evaluate() {} //Checks how good your model is
+    
+    func interactiveMode() {} //Lets the user fully interact with my program
+    
+}
--- Iris-KNN-classification/Iris-KNN-classification/Sample.swift
+++ Iris-KNN-classification/Iris-KNN-classification/Sample.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Iris {
+struct Sample {
     let features: [Double]
     let label: String
 }
